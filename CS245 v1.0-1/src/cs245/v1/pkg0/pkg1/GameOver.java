838a5db396ed250bb18103d2d5fffdbc8378dbf8
Felix Zhang <ffzhang@cpp.edu>
2017-10-08 16:31:55 -0700

    Added button functionality for GameOver screen.

--- CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/GameOver.java
+++ CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/GameOver.java
@@ -7,6 +7,8 @@ package cs245.v1.pkg0.pkg1;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  *This class is meant for the ending screen once user has finished the game
@@ -17,10 +19,37 @@ public class GameOver extends JPanel {
     private MainFrame main;
     
     public GameOver() {
-        
+        setLayout(new BorderLayout());
+        addDoneButton();
     }
     
+    /*
+    accesor method to allow panel switching from
+    within this panel.
+    */
     public void setMain(MainFrame panel) {
         this.main = panel;
     }
+    
+    
+    /*
+    adds the done button that lets the user return
+    to the main menu.
+    */
+    private void addDoneButton() {
+        JPanel buttonPanel = new JPanel();
+        
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        buttonPanel.add(done);
+                
+        done.setText("End");
+                 
+        done.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                main.swapView("menu");
+            }   
+        });        
+        add(buttonPanel, BorderLayout.PAGE_END);        
+    }   
 }
--- CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/MainFrame.java
+++ CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/MainFrame.java
@@ -9,7 +9,8 @@ import javax.swing.*;
  *
  * @author FelixZhang
  */
-public class MainFrame  extends JFrame{
+public class MainFrame extends JFrame {
+
     private CardLayout layout = new CardLayout();
     private JPanel mainP = new JPanel(layout);
     private MainMenu mainMenu = new MainMenu();
@@ -19,56 +20,63 @@ public class MainFrame  extends JFrame{
     private PlayGame play = new PlayGame();
     private GameOver over = new GameOver();
     private Timer timer;
-    
+
     /*
     Constructor for main frame.
-    */
+     */
     public MainFrame() {
         initUI();
     }
-    
+
     /*
     accessor method for sub classes to access mainMenu class
     to stop the timer for the main menu animation once the 
     user leaves the main menu.
-    */
+     */
     public void stopMenuTimer() {
         mainMenu.stopTimer();
     }
-    
+
+    /*
+    This is used by the sub panels inside the layout to be able to change to
+    other panels. Not sure if this is the correct way to do this but it works.
+     */
+    public void swapView(String key) {
+        layout.show(mainP, key);
+    }
+
     /*
     accessor method for sub classes to access mainMenu class 
     method to start the timer for the main menu animation once 
     the user returns to the main menu.
-    */
+     */
     public void startMenuTimer() {
         mainMenu.startTimer();
     }
 
     /*
     initializer method to draw the frame and add elements to it.
-    */
+     */
     private void initUI() {
-        
+        ImageIcon icon = new ImageIcon("thinking.jpg");
+
         startLayout();
         introTimer();
-        
-        
-        setTitle("PlaceHolder title");
+
+        setTitle("Thinkman's Hangman");
         setSize(600, 400);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
-        
+        setIconImage(icon.getImage());
+
         add(mainP);
         pack();
-        //pack(); idk if i actually need this yet.
     }
-    
+
     /*
     I moved the panel initializations into another method
     to clean up the initUI() method.
-    */
+     */
     private void startLayout() {
         mainP.add(title, "title");
         mainP.add(mainMenu, "menu");
@@ -76,47 +84,35 @@ public class MainFrame  extends JFrame{
         mainP.add(credit, "credits");
         mainP.add(play, "play");
         mainP.add(over, "over");
-        
+
         mainMenu.setMain(this);
         credit.setMain(this);
         hScore.setMain(this);
         play.setMain(this);
         over.setMain(this);
-
     }
-    
+
     /*
     Timer to set the 3 second delay between the title panel
     and the main menu.
-    */
+     */
     private void introTimer() {
-        
         int delay = 3000;
-        
+
         timer = new Timer(delay, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                
                 layout.show(mainP, "menu");
             }
-            
         });
         timer.setRepeats(false);
         timer.start();
     }
-    
-    /*
-    This is used by the sub panels inside the layout to be able to change to
-    other panels. Not sure if this is the correct way to do this but it works.
-    */
-    public void swapView(String key) {
-        layout.show(mainP, key);
-    }
-    
+
     /**
-     * Main method that uses EventQueue.invokeLater in order to 
-     * have the GUI execute in the Event Dispatch Thread so backend
-     * calculations do not cause the GUI to freeze or lag.
+     * Main method that uses EventQueue.invokeLater in order to have the GUI
+     * execute in the Event Dispatch Thread so backend calculations do not cause
+     * the GUI to freeze or lag.
      */
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
@@ -124,5 +120,4 @@ public class MainFrame  extends JFrame{
             menu.setVisible(true);
         });
     }
-    
 }
