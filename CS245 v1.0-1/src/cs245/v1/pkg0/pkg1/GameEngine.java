f77eaf7aba6da4e303bd1db3694ecbf9c9863af8
gdgeminiano <geriannag96@gmail.com>
2017-10-10 15:05:06 -0700

    Update GameEngine.java

--- CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/GameEngine.java
+++ CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/GameEngine.java
@@ -1,5 +1,5 @@
 /***************************************************************
-* file: MaineMenu.java
+* file: GameEngine.java
 * author: Brandon Nguyen, Charly Dang, Colin Koo, Felix Zhang, Gerianna Geminiano
 * class: CS 245 – Programming Graphical User Interface
 *
@@ -14,169 +14,193 @@
 *
 ****************************************************************/ 
 package cs245.v1.pkg0.pkg1;
+import java.io.*;
+import java.util.Random;
 
-import java.awt.*;
-import javax.swing.*;
-import java.awt.event.*;
-
-
-/**
- *Main Controller class for all of the FrontEnd.
- * includes a card layout to easily switch between different
- * JPanels.
- */
-public class MainMenu extends JPanel {
-    
-    private JButton play = new JButton("Play");
-    private JButton hScore = new JButton("High Score");
-    private JButton credit = new JButton("Credits");
-    private JPanel menuButtons = new JPanel();
-    private JPanel buttons = new JPanel();
-    private CardLayout slideShow = new CardLayout();
-    private JPanel pictures = new JPanel(slideShow);
-    private boolean show = false;
-    private Timer timer;
+public class GameEngine {
+    private int finalScore;
+    private int wordLength;
     private MainFrame main;
-    
-    /*
-    Constructor for the mainMenu class
+    private boolean won;
+    //private 
+            String word;
+    private String[] wordList = {"abstract", "cemetery", "nurse", "pharmacy", "climbing"};
+
+    //Constructor
+    public GameEngine() {
+        pickWord();
+    }
+
+/*
+    method: setWin
+    puspose: sets the boolean won, if user won
     */
-    public MainMenu() {
-        setBackground(Color.black);
-        pictures.setBackground(Color.black);
-        setLayout(new BorderLayout());
-        
-        add(menuBtns(), BorderLayout.LINE_END);
-        add(pictures, BorderLayout.LINE_START);
-        
-        displayLogo();  
+    public void setWin(boolean won) {
+        this.won = won;
+    }
+/*
+    method: isWonnered
+    purpose: returns a boolean if player won or not, won = true, lost = false
+    */
+    public boolean isWonnered() {
+        return won;
+    }
+
+    /*
+    method: pickWord
+    purpose: picks a new word for the game and updates the length of the word
+    */
+    public void pickWord() {
+        int rng = new Random().nextInt(wordList.length);       
+        word = wordList[rng];
+    }
+
+    /*
+    method: setScore
+    purpose: setter for the score variable, sets the final score for the
+    end of the game.
+    */
+    public void setScore(int score) {
+        finalScore = score;
+    }
+
+    
+ /*/*
+    method: getScore
+    purpose: getter for the score variable, gets the final score for the
+    end of the game.
+    */
+    public int getScore() {
+        return finalScore;
     }
     
     /*
-    accessor method for subclasses to be able to set the
-    current instance of the MainMenu to be able to access
-    certain methods from a subclass.
-    */
-    public void setMain(MainFrame panel) {
-        this.main = panel;
-    
-    }
-    
-    /*
-    accessor method for subclasses to be able to stop the timer
-    once the user leaves the main menu so the animation does not keep playing.
-    */
-    public void stopTimer() {
-        timer.stop();
-    }
-    
-    /*
-    accesssor method for sublcasses to be able to stop the timer once the user leaves
-    the main menu so the animation does not keep going.
-    */
-    public void startTimer() {
-        timer.start();
-    }
-    
-    
-    /*
-    I used a java swing timer to slideshow the two 
-    logo pictures.
-    */
-    //NEED TO CHANGE TO USE BUFFERED IMAGE FOR AUTOMATIC IMAGE SCALING.
-    private void displayLogo() {
-        ImageIcon img = new ImageIcon(new ImageIcon("thunking.png").getImage().getScaledInstance(375, 375, Image.SCALE_SMOOTH));
-        JLabel label = new JLabel("", img, JLabel.CENTER);
-        ImageIcon img2 = new ImageIcon(new ImageIcon("thunking2.png").getImage().getScaledInstance(375, 375, Image.SCALE_SMOOTH));
-        JLabel label2 = new JLabel("", img2, JLabel.CENTER);
-        
-        pictures.add(label, "one");
-        pictures.add(label2, "two");
-        
-        timer = new Timer(1000, new ActionListener() {
+    method: updateHighScore
+    purpose: Gets players score and adds it to the highscore list if its in the top 5
+    First it reads the highscores file, adds it to an array with the new score, and 
+    then overrites old file.
+     */
+    public void updateHighScore(String name, int score) {
+
+         String scoreArr[] = new String[5];
+        try {
+            File f = new File("HighScores.txt");
+            BufferedReader br = new BufferedReader(new FileReader(f));
+
             
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (show) {
-                    slideShow.show(pictures, "one");
-                    show = false;
+            boolean replaced = false;
+            int i = 0;
+            // if File is empty
+           String line = br.readLine();
+            if (line.length() == 0 ) {  
+                scoreArr[i] = name + " " + Integer.toString(score);
+                ++i;
+            } else {
+                while ((line) != null) {
+                String[] splitted = line.split(" ");
+                if ((Integer.parseInt(splitted[1]) <= score) && replaced == false) {
+                    if (i < 5) {
+                        scoreArr[i] = name + " " + Integer.toString(score);
+                        ++i;
+                    }
+                    if (i < 5) {
+                        scoreArr[i] = line;
+                        ++i;
+                    }
+                    replaced = true;
                 } else {
-                    slideShow.show(pictures, "two");
-                    show = true;
+                    if (i < 5) {
+                        scoreArr[i] = line;
+                        ++i;
+                    }
+                }
+                line = br.readLine();
+            }
+            }
+            if (i < 5) {
+                while (i < 5) {
+                    System.out.println(i);
+                    scoreArr[i] = "AAA 0";
+                    ++i;
                 }
             }
-            
-        });
-        timer.start();
+            i = 0;
+
+            br.close();
+            f.delete();
+            BufferedWriter bw = new BufferedWriter(new FileWriter("HighScores.txt"));
+
+            for (int n = 0; n < scoreArr.length; n++) {
+
+                bw.write(scoreArr[n]);
+                bw.newLine();
+                bw.flush();
+            }
+
+            bw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
+
+
     /*
-    Method that sets the main menu buttons and adds functions
-    to them.
+    method: getLetterPositions
+    purpose: Returns the letter position in the secret word string.
     */
-    private JComponent menuBtns() {
-        
-        //setting layout of inner panel
-        menuButtons.setLayout(new BorderLayout());
+    public int[] getLetterPositions(String input) {
+    	char letter = Character.toLowerCase(input.charAt(0));
+        int[] positions = new int[frequency(letter)];
+        int counter = 0;
+        for(int i = 0; i < getWordLength(); i++){
+        	if(letter == word.charAt(i)){
+        		positions[counter] = i;
+        		counter++;
+        	}
+        }
+        return positions;
+    }
+    /*
+    method: frequency
+    purpose: Returns the number of times a char is found in a string. Then returns
+      that as an int.
+     */
+    public int frequency(char letter){
+    	int counter = 0;
+    	for( int i = 0; i < getWordLength(); i++ ) {
+    	    if( word.charAt(i) == letter ) {
+    	        counter++;
+    	    }
+    	}
+    	return counter;
+    }
 
-        //setting up layout of panel that goes inside menuButtons
-        buttons.setPreferredSize(new Dimension(200, 150));
-        buttons.setLayout(new BoxLayout(buttons, BoxLayout.Y_AXIS));
-        buttons.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+    /*
+    method: getWordLength
+    puspose: Returns the length of the secret word.
+    */
+    public int getWordLength() {
+        return word.length();
+    }
 
-        buttons.add(play);
-        buttons.add(Box.createRigidArea(new Dimension(0, 10)));
-        buttons.add(hScore);
-        buttons.add(Box.createRigidArea(new Dimension(0, 10)));
-        buttons.add(credit);
+    /*
+    method:containsLetter
+    purpose: Checks if the letter pressed is contained
+    inside the secret word string.
+    */
+    public boolean containsLetter(String input) {
+    	boolean contains = false;
+    	char letter = Character.toLowerCase(input.charAt(0)) ;
+        
+        
+    	for(int i = 0; i < getWordLength(); i++){
+    		if(word.charAt(i) == letter){
+    			contains = true;
+    		}
+    	}
+        return contains;
+    }
 
-        //resizing the buttons so it looks like the example
-        //also setting all of the buttons to be the same size
-        hScore.setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
-        play.setMaximumSize(hScore.getMaximumSize());
-        credit.setMaximumSize(hScore.getMaximumSize());
 
-        //right aligningt the buttons
-        play.setAlignmentX(Component.RIGHT_ALIGNMENT);
-        hScore.setAlignmentX(Component.RIGHT_ALIGNMENT);
-        credit.setAlignmentX(Component.RIGHT_ALIGNMENT);
-
-        //finally add the inner panel to menu panel and return.
-        menuButtons.add(buttons, BorderLayout.PAGE_END);
-        
-        
-        // Action Listeners for the 3 main buttons.
-        hScore.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                stopTimer();
-                main.swapView("High Score");
-            }
-        });
-        
-        credit.addActionListener(new ActionListener() {
-            
-            public void actionPerformed(ActionEvent e) {
-                stopTimer();
-                main.swapView("credits");
-            }
-        });
-        
-        
-        play.addActionListener(new ActionListener() {
-            
-            public void actionPerformed(ActionEvent e) {
-                stopTimer();
-                main.startNewGame();
-                main.swapView("play");
-            }
-        });
-        
-        menuButtons.setBackground(Color.black);
-        buttons.setBackground(Color.black);
-        menuButtons.setOpaque(false);
-        buttons.setOpaque(false);
-        
-        return menuButtons;
-    } 
 }
