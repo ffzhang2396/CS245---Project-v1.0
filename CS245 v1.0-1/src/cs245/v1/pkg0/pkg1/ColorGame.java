522ea825256d9917f865fb55fc10c8519c943130
Felix Zhang <ffzhang@cpp.edu>
2017-10-16 23:55:01 -0700

    Started work on drawing the circles.

--- CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/ColorGame.java
+++ CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/ColorGame.java
@@ -13,9 +13,54 @@ import javax.swing.*;
  * @author FelixZhang
  */
 public class ColorGame extends JPanel {
-        
+    CirclePanel game = new CirclePanel();
+    ColorGameEngine engine = new ColorGameEngine();
+
     public ColorGame() {
+        setLayout(new BorderLayout());
+        
+
+    }
     
-}
     
+    
+    
+    /*
+    inner panel that contains the circles. Game engine
+    is supposed to calculate the circle positions and this 
+    nested class just draws them.
+    */
+    private static class CirclePanel extends JPanel {
+        private boolean done = false;
+        
+        
+        
+        
+        
+        public void paintComponent(Graphics g) {
+          super.paintComponent(g);
+          Graphics2D g2 = (Graphics2D) g;
+          
+          g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+          
+          g2.setStroke(new BasicStroke(1));
+          
+          g2.drawOval(0, 0, 50, 50);
+          
+          
+          while (!done) {
+              
+          }
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+
 }
--- CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/ColorGameEngine.java
+++ CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/ColorGameEngine.java
@@ -5,10 +5,94 @@
  */
 package cs245.v1.pkg0.pkg1;
 
+import java.util.Random;
+
 /**
  *
  * @author FelixZhang
  */
 public class ColorGameEngine {
-    
+
+    Circle[] circles = new Circle[5];
+
+    public void drawCircles() {
+
+    }
+
+    /*
+    This is used to calculate the positions of the circles with help
+    from the intersects method.
+     */
+    private void calcCirclePos(int xHeight, int yHeight) {
+        int windowH = yHeight;
+        int windowW = xHeight;
+        Random rand = new Random();
+        int tempX, tempY;
+        boolean done = false;
+
+    }
+
+    /*
+    object to hold the circle information.
+     */
+    private static class Circle {
+
+        private int xPos, yPos;
+        private int xCenter, yCenter;
+        private final int radius = 20;
+
+        public Circle(int xPos, int yPos) {
+            this.xPos = xPos;
+            this.yPos = yPos;
+            xCenter = xPos + 10;
+            yCenter = yPos + 10;
+        }
+
+        /*
+    checks if two circles are intersecting given the two
+    circle objects. NEEDS TO BE CHECKED IF ACTUALLY WORKS.
+         */
+        public boolean intersects(Circle comp) {
+            int r0 = 0;
+            int r1 = (int) Math.pow(this.getRadius(), 2);
+            int x0x1 = (int) Math.pow(this.xCenter - comp.getXCenter(), 2);
+            int y0y1 = (int) Math.pow(this.yCenter - comp.getYCenter(), 2);
+            int sum = x0x1 + y0y1;
+
+            if ((r0 <= sum) && (sum <= r1)) {
+                return true;
+            }
+            return false;
+        }
+
+        public int getXCenter() {
+            return xCenter;
+        }
+
+        public int getYCenter() {
+            return yCenter;
+
+        }
+
+        public int getXPos() {
+            return xPos;
+        }
+
+        public int getYPos() {
+            return yPos;
+        }
+
+        public int getRadius() {
+            return radius;
+        }
+
+        public void setXPos(int xPos) {
+            this.xPos = xPos;
+        }
+
+        public void setYPos(int yPos) {
+            this.yPos = yPos;
+        }
+
+    }
 }
