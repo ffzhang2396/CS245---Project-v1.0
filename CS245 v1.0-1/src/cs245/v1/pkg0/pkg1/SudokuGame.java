9a36e98ff5bddf370a43799d7153242dcff6569e
Felix Zhang <ffzhang@cpp.edu>
2017-10-24 18:54:45 -0700

    Started working on the framework on the sudoku game.

--- CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/SudokuGame.java
+++ CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/SudokuGame.java
@@ -6,37 +6,109 @@
 package cs245.v1.pkg0.pkg1;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 /**
  *
- * 
+ *
  */
 public class SudokuGame extends JPanel {
+
     private SudokuGameEngine engine;
     private MainFrame main;
+    private JTextField[][] boxes = new JTextField[9][9];
     private JPanel board = new JPanel();
-    
-    
+
     public SudokuGame(SudokuGameEngine engine) {
         this.engine = engine;
         setLayout(new BorderLayout());
     }
-    
+
     public void setMain(MainFrame main) {
         this.main = main;
     }
-    
-    
+
     /*
     For the sudoku board we are going to use a
     9x9 gridLayout for the inner JPanel thats going to be in the center of the 
     outer JPanel which is an instance of this class.
-    */
+     */
     private void createBoard() {
-        
+
+    }
+
+    /*
+    This method draws the title bar of the game panel.
+    Title bar should include the time as well as a stylized
+    version of the game name. 
+     */
+    private void titleBar() {
+        JPanel title = new JPanel(new BorderLayout());
+
+    }
+
+    /*
+    adds in the submit button that goes on the left
+    side of the game board.
+     */
+    private void submitButton() {
+    }
+
+    /*
+    This method adds in the submit button that goes on the right
+    side of the game board. 
+     */
+    private void quitButton() {
+
     }
     
+    /*
+    initializes the board to a new state.
+    Probably can use this to restart the game.
+    */
+    private void initBoard() {
+        int[][] board = engine.getBoard();
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                boxes[i][j] = new JTextField();
+                if (board[i][j] != -1) {
+                    boxes[i][j].setText(Integer.toString(board[i][j]));
+                    boxes[i][j].setEditable(false);                   
+                } else {
+                    boxes[i][j].setText("");
+                    boxes[i][j].setEditable(true);
+                }
+            }
+        }
+    }
     
-    
+    /*
+    need to add a separate action listener that listens to 
+    when the JtextFields are modified with user input.
+    This listener needs to be different so that it does not 
+    mess with the action listeners for the buttons.
+    */
+    private class gridListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JTextField src = (JTextField) e.getSource();
+            
+            
+            
+        }
+        
+        /*
+        this method is used for finding the x and y values
+        for the engine to check if the value is a valid input.
+        */
+        private void findSrc() {
+            
+        }
+        
+    }
+
 }
--- CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/SudokuGameEngine.java
+++ CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/SudokuGameEngine.java
@@ -10,5 +10,60 @@ package cs245.v1.pkg0.pkg1;
  * @author dieha
  */
 public class SudokuGameEngine {
+
+    private int[][] answer = {
+        {8, 3, 5, 4, 1, 6, 9, 2, 7},
+        {2, 9, 6, 8, 5, 7, 4, 3, 1},
+        {4, 1, 7, 2, 9, 3, 6, 5, 8},
+        {5, 6, 9, 1, 3, 4, 7, 8, 2},
+        {1, 2, 3, 6, 7, 8, 5, 4, 9},
+        {7, 4, 8, 5, 2, 9, 1, 6, 3},
+        {6, 5, 2, 7, 8, 1, 3, 9, 4},
+        {9, 8, 1, 3, 4, 5, 2, 7, 6},
+        {3, 7, 4, 9, 6, 2, 8, 1, 5}};
+
+    //initial starting board, -1 means the 
+    //box should be empty.
+    private int[][] board = {
+        {8, -1, -1, 4, -1, 6, -1, -1, 7},
+        {-1, -1, -1, -1, -1, -1, 4, -1, -1},
+        {5, -1, 9, -1, -1, 3, -1, 7, 8, -1},
+        {-1, -1, -1, -1, 7, -1, -1, -1, -1},
+        {-1, 4, 8, -1, 2, -1, 1, -1, 3},
+        {-1, 5, 2, -1, -1, -1, -1, 9, -1},
+        {-1, -1, 1, -1, -1, -1, -1, -1, -1},
+        {3, -1, -1, 9, -1, 2, -1, -1, 5},};
+
+    //used to update the backend board. 
+    public void updateBoard(int[][] newBoard) {
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                board[i][j] = newBoard[i][j];
+            }
+        }
+    }
+
+    /*
+    used to return the board so the GUI can use it
+    to populate the visual.
+    */
+    public int[][] getBoard() {
+        return board;
+    }
     
+    /*
+    This method is called whenver the user enters a number
+    into the text field to check if it is a valid input.
+    */
+    public boolean checkInput(int row, int col) {
+        boolean rowValid = false;
+        boolean colValid = false;
+        
+        for (int i = 0; i < board.length; i++) {
+            
+        }       
+        return false;
+    }
+
 }
