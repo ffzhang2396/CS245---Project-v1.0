c6d58b855fedcc909266e2c7b5332de1c7d993db
charlydang <charly_dang@yahoo.com>
2017-10-27 14:27:25 -0700

    Update GameOver.java

--- CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/GameOver.java
+++ CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/GameOver.java
@@ -1,5 +1,5 @@
 /** *************************************************************
- * file: HManGame.java
+ * file: GameOver.java
  * author: Brandon Nguyen, Charly Dang, Colin Koo, Felix Zhang, Gerianna Geminiano
  * class: CS 245 � Programming Graphical User Interface
  *
@@ -18,94 +18,175 @@ package cs245.v1.pkg0.pkg1;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.awt.geom.Line2D;
-import java.text.DateFormat;
-import java.util.Date;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 /**
- * This will the the actual GUI for the game. I implemented action listener so
- * we can just use one action listener instead of creating a separate listener
- * for every JButton for each letter of the alphabet. This class will also be
- * the controller for the backend, so game update methods should be written in
- * here for backend communication.
- *
+ * This class is meant for the ending screen once user has finished the game or
+ * has clicked the skip button.
  */
-public class HManGame extends JPanel implements ActionListener {
+public class GameOver extends JPanel {
 
-    private int score = 100;
+    private JButton done = new JButton();
+    private JTextField input = new JTextField(10);
+    private JLabel userPrompt = new JLabel();
+    private JLabel namePrompt = new JLabel();
+    private JPanel titlePanel = new JPanel();
+    private JPanel containerPanel = new JPanel();
+    private JPanel buttonPanel = new JPanel();
     private MainFrame main;
-    private GamePanel game = new GamePanel();
-    private HManGameEngine engine;
-    private JPanel btnPanel = new JPanel(new GridLayout(2, 13, 5, 5));
-    private JPanel titleBar = new JPanel(new BorderLayout());
-    private JPanel centerPanel = new JPanel(new BorderLayout());
-    private JPanel skipPanel = new JPanel();
-    private JButton skip = new JButton("Skip");
-    private JLabel points = new JLabel();
-    private JLabel[] guessWord;
-    private JButton[] buttons = new JButton[26];
-    private String[] letters = {"A", "B", "C", "D", "E", "F",
-        "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
-        "S", "T", "U", "V", "W", "X", "Y", "Z"};
-    
+    private ColorGameEngine engine;
+    private HManGame game;
 
-    /*Constructor
-    for Game UI panel.
+    /*
+    Constructor
      */
-    public HManGame(HManGameEngine engine) {
-
+    public GameOver(ColorGameEngine engine) {
         this.engine = engine;
         setLayout(new BorderLayout());
-        loadUI();
-        requestFocusInWindow();
-        checkEscape();
-
     }
 
     /*
-    method: loadUI
-    purpose: adds all of the panels in this panel to the screen
-    to display them.
+    method: setMain
+    purpose:accesor method to allow panel switching from
+    within this panel.
      */
-    private void loadUI() {
-
-        addButtons();
-        add(titleBar, BorderLayout.PAGE_START);
-        add(centerPanel, BorderLayout.CENTER);
-        add(btnPanel, BorderLayout.PAGE_END);
-        add(skipPanel, BorderLayout.LINE_END);
-        drawTitle();
-        drawGame();
-        skipButton();
-        
+    public void setMain(MainFrame panel) {
+        this.main = panel;
     }
 
     /*
-    method: startNewGame()
-    purpose: resets variables and display for new game
+    method: addTitle
+    purpose: adds the title to game over panel
      */
-    public void startNewGame() {
-        game.resetCount(); //reset # of wrong tries
-        score = 100; // reset the score back to 100
-        engine.pickWord(); // pick a secret word
-        centerPanel.removeAll();
-        titleBar.removeAll();
-        drawTitle();
-        drawGame();
+    public void addTitle() {
 
-        //enable all of the buttons
-        for (int i = 0; i < buttons.length; i++) {
-            buttons[i].setEnabled(true);
+        JLabel title = new JLabel();
+        titlePanel.removeAll();
+        // Winner = 1,2, or 3 depending on win status
+        int winner = engine.isWinner();
+        // If payer wins and isn't in top 5 highscore
+        if (winner == 2) {
+            title.setText("<html>Congratulations! You win!<br> But you didn't get a highscore</html>");
+            title.setToolTipText("Better luck next time!");
+            buttonPanel.removeAll();
+            addDoneButton();
+        } // Else if player wins but is in top 5 highscore
+        else if (winner == 3) {
+            title.setText("Congratulations! You win!");
+            title.setToolTipText("Almost as good as me!");
+            buttonPanel.removeAll();
+            inputUserScore();
+        } // Else if player loses
+        else if (winner == 1) {
+            title.setText("Aww you didnt win. Try again!");
+            containerPanel.removeAll();
+            addDoneButton();
         }
+
+        title.setFont(new Font("Kristen ITC", Font.BOLD, 30));
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        titlePanel.add(title);
+        add(titlePanel, BorderLayout.PAGE_START);
+
     }
 
     /*
-    method: skipButton
-    purpose: skip button to end game functionality needs
-    to be implemented here.
+    method: inputUserScore
+    purpose: Allows user to input their name and score if they get a high score
      */
-    private void skipButton() {
+    private void inputUserScore() {
+        containerPanel.removeAll();
+        userPrompt = new JLabel();
+        namePrompt = new JLabel();
+        input = new JTextField(10);
+        String scoreText;
+        scoreText = "Woo! You made the High Score! Score: " + Integer.toString(engine.getScore());
+        JPanel inputPanel = new JPanel();
+
+        JButton yes = new JButton("Yes");
+        yes.setToolTipText("Add Score");
+        JButton no = new JButton("No");
+        no.setToolTipText("Do not add score!");
+        JButton confirm = new JButton("OK");
+        confirm.setToolTipText("OK");
+        JLabel notice = new JLabel(scoreText, SwingConstants.CENTER);
+        userPrompt.setText("Would you like to save score to High Score list?");
+        namePrompt.setText("Input your initials: ");
+        notice.setFont(new Font("Kristen ITC", Font.BOLD, 20));
+        notice.setToolTipText("Notice me Senpai");
+        containerPanel.setLayout(new BorderLayout());
+        inputPanel.setLayout(new FlowLayout());
+        input.setVisible(false);
+        confirm.setVisible(false);
+        namePrompt.setVisible(false);
+        namePrompt.setToolTipText("Please Enter your Initials");
+        userPrompt.setToolTipText("Good job! How about entering your score?");
+
+        inputPanel.add(userPrompt);
+        inputPanel.add(namePrompt);
+        inputPanel.add(yes);
+        inputPanel.add(no);
+        inputPanel.add(input);
+        inputPanel.add(confirm);
+
+        notice.setAlignmentX(CENTER_ALIGNMENT);
+        notice.setAlignmentY(GameOver.CENTER_ALIGNMENT);
+        containerPanel.setBorder(BorderFactory.createEmptyBorder(50, 0, 0, 0));
+        inputPanel.setAlignmentX(CENTER_ALIGNMENT);
+        inputPanel.setAlignmentY(GameOver.CENTER_ALIGNMENT);
+
+        // yes button
+        yes.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                yes.setVisible(false);
+                no.setVisible(false);
+                userPrompt.setVisible(false);
+                namePrompt.setVisible(true);
+                confirm.setVisible(true);
+                input.setVisible(true);
+            }
+        });
+
+        // no button
+        no.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                yes.setVisible(false);
+                no.setVisible(false);
+                userPrompt.setVisible(false);
+                main.swapView("menu");
+                main.updateScore();
+            }
+        });
+
+        // ok button
+        confirm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String inputValue = input.getText();
+                if (inputValue.length() == 0) {
+                    inputValue = "NONAME";
+                }
+                engine.updateHighScore(inputValue, engine.getScore());
+                main.swapView("menu");
+                main.updateScore();
+            }
+        });
+        containerPanel.add(notice, BorderLayout.PAGE_START);
+        containerPanel.add(inputPanel, BorderLayout.CENTER);
+        add(containerPanel, BorderLayout.CENTER);
+    }
+
+    /*
+    method: addDoneButton
+    purpose:adds the done button that lets the user return
+    to the main menu.
+     */
+    private void addDoneButton() {
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
         String ACTION_KEY = "The Action";
         Action actionListener = new AbstractAction() {
         public void actionPerformed(ActionEvent actionEvent) {
@@ -115,303 +196,20 @@ public class HManGame extends JPanel implements ActionListener {
         }
         };
         KeyStroke escape = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, true);
-        InputMap inputMap = skip.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+        InputMap inputMap = done.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
         inputMap.put(escape, ACTION_KEY);
-        ActionMap actionMap = skip.getActionMap();
+        ActionMap actionMap = done.getActionMap();
         actionMap.put(ACTION_KEY, actionListener);
-        skip.setActionMap(actionMap);
-        
-        
-        skip.setToolTipText("Press this button to skip the Hangman Game and go directly to the Color Game");
-        skipPanel.add(skip);
-
-        skip.addActionListener(new ActionListener() {
+        done.setActionMap(actionMap);
+        buttonPanel.add(done);
+        done.setToolTipText("Click done to continue!");
+        done.setText("End");
+        done.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                engine.setScore(0);
-                main.gameOverMessage();
-                main.swapView("play2");
+                main.swapView("menu");
             }
         });
+        add(buttonPanel, BorderLayout.PAGE_END);
     }
-    private void checkEscape(){
-         addKeyListener(new KeyAdapter() {
-            public void keyPressed(KeyEvent ke) {  // handler
-            	if(ke.getKeyCode() == ke.VK_ESCAPE) {
-                    System.out.println("escaped ?");
-                    //setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE); // trying to close
-                    System.exit(0);
-                 } else if(ke.getKeyCode() == ke.VK_F1){
-                    System.out.println("display credit like screen");
-                  }
-           } 
-        });
-    }
-    /*
-    method: drawGame
-    purpose: Draws the visual representation of the game, which includes
-    the hangman as well as the places for the word.
-     */
-    private void drawGame() {
-        JPanel word = new JPanel();
-        int length = engine.getWordLength();
-        guessWord = new JLabel[length];
-
-        centerPanel.setLayout(new BorderLayout());
-        centerPanel.add(game, BorderLayout.CENTER);
-        word.setLayout(new FlowLayout());
-
-        for (int i = 0; i < length; i++) {
-            guessWord[i] = new JLabel(" ", SwingConstants.CENTER);
-            guessWord[i].setPreferredSize(new Dimension(30, 30));
-            guessWord[i].setFont(new Font("Arial", Font.PLAIN, 24));
-            word.add(guessWord[i]);
-            guessWord[i].setOpaque(true);
-            guessWord[i].setBorder(BorderFactory.createMatteBorder(0, 0, 3, 0, Color.black));
-            guessWord[i].setToolTipText("Guess this word!!");
-        }
-        centerPanel.add(word, BorderLayout.PAGE_END);
-    }
-
-    /*
-    method: drawTitle
-    purpose: Draws the title bar for the hangman game where it includes the stylized drawing
-    of the hangman word as well as the current date and time.
-     */
-    private void drawTitle() {
-        JLabel hangman = new JLabel("HANGMAN");
-        JLabel time = new JLabel();
-        hangman.setToolTipText("Welcome to the Hangman Game!");
-        // adding the time
-        time.setHorizontalAlignment(JLabel.CENTER);
-        time.setFont(UIManager.getFont("Label.font").deriveFont(Font.BOLD, 12));
-        Timer timer = new Timer(500, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                time.setText(DateFormat.getDateTimeInstance().format(new Date()));
-              
-            }
-        });
-        timer.setRepeats(true);
-        timer.setCoalesce(true);
-        timer.setInitialDelay(0);
-        timer.start();
-        time.setToolTipText("The current time!");
-        titleBar.add(time, BorderLayout.LINE_END);
-
-        //adding the stylized HANGMAN
-        hangman.setFont(new Font("Papyrus", Font.BOLD, 18));
-        titleBar.add(hangman, BorderLayout.LINE_START);
-
-        //adding the points
-        points.setText("Points: " + score);
-        points.setFont(new Font("Arial", Font.ITALIC, 14));
-        points.setBorder(BorderFactory.createEmptyBorder(0, 100, 0, 10));
-        points.setToolTipText("Your current total of points: " + score);
-        titleBar.add(points, BorderLayout.CENTER);
-    }
-
-    /*
-    method: addButtons
-    purpose: adds an array of JButtons that make up the alphabet buttons for the
-    user to click on when playing the game.
-     */
-    private void addButtons() {
-        btnPanel.setBorder(BorderFactory.createEmptyBorder(0, 5, 10, 5));
-        for (int i = 0; i < letters.length; i++) {
-            buttons[i] = new JButton(letters[i]);
-            buttons[i].addActionListener(this);
-            // buttons[i].setFont(new Font("Calibri", Font.PLAIN, 11));
-            buttons[i].setMargin(new Insets(0, 0, 0, 0));
-            buttons[i].setToolTipText("Press " + letters[i]);
-            btnPanel.add(buttons[i]);
-        }
-
-    }
-
-    /*
-    method:actionPerformed
-    purpose: checks the button presed against
-    the secret word as well as disable the button once the letter is added to
-    the word.
-     */
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        //if the user chooses the correct letter
-        // disable the letter and add the letter
-        // to the screen.
-        JButton button = (JButton) e.getSource();
-        if (engine.containsLetter(e.getActionCommand())) {
-            int[] positions = engine.getLetterPositions(button.getText());
-            for (int i = 0; i < positions.length; i++) {
-                guessWord[positions[i]].setText(button.getText());
-            }
-
-            button.setEnabled(false);
-            game.repaint();
-
-            if (wordFound()) {
-                engine.setScore(score);
-                gameFinished(true);
-            }
-        } else {
-            // if the user chooses the incorrect letter
-            //increment the wrong tries count and subtract 10 from score.
-            //update the count.
-            score -= 10;
-            points.setText("Points: " + score);
-            game.addCount();
-            button.setEnabled(false);
-
-            if (game.getTries() == 6) { //Game Over                
-                gameFinished(false);
-                engine.setScore(score);
-            }
-        }
-      
-    }
-
-    /*
-    method: wordFound
-    purpose: checks if the word has been filled out, if it has then end the game.
-     */
-    private boolean wordFound() {
-        boolean found = true;
-        for (int i = 0; i < guessWord.length; i++) {
-            if (guessWord[i].getText() == " ") {
-                found = false;
-            }
-        }
-        return found;
-    }
-
-    /*
-    method: gameFinished
-    purpose: This method is going to be used to transition into the
-    game over panel.
-     */
-    private void gameFinished(boolean won) {
-        for (int i = 0; i < buttons.length; i++) {
-            buttons[i].setEnabled(false);
-        }
-
-        // Timer delay for the game over screen.
-        // Probably need to change this to go to the next game
-        // After user has finished the game.
-        Timer timer = new Timer(1500, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                engine.setWin(won);
-                engine.setScore(score);
-                main.gameOverMessage();
-                main.swapView("play2");
-            }
-        });
-        timer.setRepeats(false);
-        timer.start();
-    }
-
-    /*
-    method: setMain
-    purpose: setting the current mainFrame to the reference in this class
-    to allow for panel switching.
-     */
-    public void setMain(MainFrame panel) {
-        this.main = panel;
-    }
-
-    /*
-    Inner class to utilize the paint() method in order to draw the
-    hangman as well as to draw limbs onto the screen once the user
-    has reached a certain number of incorrect tries.disc
-     */
-    private static class GamePanel extends JPanel {
-
-        private int wrongTries;
-
-        public GamePanel() {
-
-        }
-
-        /*
-        method: getTries()
-        purpose: returns the number of wrong tries the user 
-        has attempted.
-         */
-        public int getTries() {
-            return wrongTries;
-        }
-
-        /*
-        method: addCount
-        purpose: increments the number of wrong tries.
-         */
-        public void addCount() {
-            wrongTries++;
-        }
-
-        /*
-        method: resetCount
-        purpose: resets the number of wrong tries.
-         */
-        public void resetCount() {
-            wrongTries = 0;
-        }
-
-        /*
-        method: paintComponent
-        purpose: Draws the hangman graphic and updates it based
-        on the number of wrong tries the user has.
-         */
-        public void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            Graphics2D g2 = (Graphics2D) g;
-            super.setToolTipText("The gallows demand a body! Number of wrong guesses: " + getTries() + "!");
-            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-            g2.setStroke(new BasicStroke(10));
-            Line2D base = new Line2D.Double(500, 250, 80, 250); // base
-            Line2D vLine = new Line2D.Double(400, 250, 400, 20); // vertical line for pole
-            Line2D hLine = new Line2D.Double(400, 20, 300, 20); // horizontal line for beam
-            Line2D hangRope = new Line2D.Double(300, 20, 300, 80); // rope
-
-            g2.draw(base);
-            g2.draw(vLine);
-            g2.draw(hLine);
-            g2.setStroke(new BasicStroke(1));
-            g2.draw(hangRope);
-
-            g2.setStroke(new BasicStroke(4));
-
-            if (wrongTries >= 1) {
-                g2.drawOval(279, 75, 20, 20);
-            }
-
-            if (wrongTries >= 2) {
-                g2.drawLine(299, 90, 299, 150);
-            }
-
-            g2.setStroke(new BasicStroke(3));
-
-            if (wrongTries >= 3) {
-                g2.drawLine(299, 100, 325, 125);
-            }
-
-            if (wrongTries >= 4) {
-                g2.drawLine(299, 100, 273, 110);
-            }
-
-            if (wrongTries >= 5) {
-                g2.drawLine(299, 150, 326, 170);
-            }
-
-            if (wrongTries >= 6) {
-                g2.drawLine(299, 150, 283, 180);
-            }
-            
-            repaint();
-        }
-
-    }
-
 }
