822fe30a9891ecdc69adcd0644f0314849d18f27
charlydang <charly_dang@yahoo.com>
2017-10-27 14:25:45 -0700

    Update GameOver.java

--- CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/GameOver.java
+++ CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/GameOver.java
@@ -1,7 +1,7 @@
 /** *************************************************************
  * file: GameOver.java
  * author: Brandon Nguyen, Charly Dang, Colin Koo, Felix Zhang, Gerianna Geminiano
- * class: CS 245 – Programming Graphical User Interface
+ * class: CS 245 � Programming Graphical User Interface
  *
  * assignment: Swing Project v1.1
  * date last modified: 10/19/17
@@ -20,6 +20,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 /**
  * This class is meant for the ending screen once user has finished the game or
@@ -68,11 +69,13 @@ public class GameOver extends JPanel {
         // If payer wins and isn't in top 5 highscore
         if (winner == 2) {
             title.setText("<html>Congratulations! You win!<br> But you didn't get a highscore</html>");
+            title.setToolTipText("Better luck next time!");
             buttonPanel.removeAll();
             addDoneButton();
         } // Else if player wins but is in top 5 highscore
         else if (winner == 3) {
             title.setText("Congratulations! You win!");
+            title.setToolTipText("Almost as good as me!");
             buttonPanel.removeAll();
             inputUserScore();
         } // Else if player loses
@@ -103,18 +106,23 @@ public class GameOver extends JPanel {
         JPanel inputPanel = new JPanel();
 
         JButton yes = new JButton("Yes");
+        yes.setToolTipText("Add Score");
         JButton no = new JButton("No");
+        no.setToolTipText("Do not add score!");
         JButton confirm = new JButton("OK");
+        confirm.setToolTipText("OK");
         JLabel notice = new JLabel(scoreText, SwingConstants.CENTER);
         userPrompt.setText("Would you like to save score to High Score list?");
         namePrompt.setText("Input your initials: ");
         notice.setFont(new Font("Kristen ITC", Font.BOLD, 20));
-
+        notice.setToolTipText("Notice me Senpai");
         containerPanel.setLayout(new BorderLayout());
         inputPanel.setLayout(new FlowLayout());
         input.setVisible(false);
         confirm.setVisible(false);
         namePrompt.setVisible(false);
+        namePrompt.setToolTipText("Please Enter your Initials");
+        userPrompt.setToolTipText("Good job! How about entering your score?");
 
         inputPanel.add(userPrompt);
         inputPanel.add(namePrompt);
@@ -179,7 +187,22 @@ public class GameOver extends JPanel {
      */
     private void addDoneButton() {
         buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        String ACTION_KEY = "The Action";
+        Action actionListener = new AbstractAction() {
+        public void actionPerformed(ActionEvent actionEvent) {
+        JButton source = (JButton) actionEvent.getSource();
+        System.exit(0);
+        //System.out.println("Activated: " + source.getText());
+        }
+        };
+        KeyStroke escape = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, true);
+        InputMap inputMap = done.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+        inputMap.put(escape, ACTION_KEY);
+        ActionMap actionMap = done.getActionMap();
+        actionMap.put(ACTION_KEY, actionListener);
+        done.setActionMap(actionMap);
         buttonPanel.add(done);
+        done.setToolTipText("Click done to continue!");
         done.setText("End");
         done.addActionListener(new ActionListener() {
             @Override
