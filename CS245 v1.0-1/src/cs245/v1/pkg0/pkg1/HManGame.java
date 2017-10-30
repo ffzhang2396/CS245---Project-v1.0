230d440b62956f5feecada5d02a98298318390bc
charlydang <charly_dang@yahoo.com>
2017-10-29 19:36:28 -0700

    Update HManGame.java

--- CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/HManGame.java
+++ CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/HManGame.java
@@ -1,7 +1,7 @@
 /** *************************************************************
  * file: HManGame.java
  * author: Brandon Nguyen, Charly Dang, Colin Koo, Felix Zhang, Gerianna Geminiano
- * class: CS 245 � Programming Graphical User Interface
+ * class: CS 245 – Programming Graphical User Interface
  *
  * assignment: Swing Project v1.1
  * date last modified: 10/19/17
@@ -48,8 +48,7 @@ public class HManGame extends JPanel implements ActionListener {
     private String[] letters = {"A", "B", "C", "D", "E", "F",
         "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
         "S", "T", "U", "V", "W", "X", "Y", "Z"};
-    private JFrame frame;
-    
+	private JFrame frame;
 
     /*Constructor
     for Game UI panel.
@@ -59,8 +58,6 @@ public class HManGame extends JPanel implements ActionListener {
         this.engine = engine;
         setLayout(new BorderLayout());
         loadUI();
-        requestFocusInWindow();
-       
 
     }
 
@@ -79,7 +76,6 @@ public class HManGame extends JPanel implements ActionListener {
         drawTitle();
         drawGame();
         skipButton();
-        
     }
 
     /*
@@ -107,12 +103,12 @@ public class HManGame extends JPanel implements ActionListener {
     to be implemented here.
      */
     private void skipButton() {
-        String ACTION_KEY = "The Action";
+		
+		String ACTION_KEY = "The Action";
         Action actionListener = new AbstractAction() {
         public void actionPerformed(ActionEvent actionEvent) {
         String source = actionEvent.getActionCommand();
-        System.out.println("The command:" + source);
-        if(source.equals(" ")){
+        if(source==null){
             JOptionPane.showMessageDialog(frame, "Winter Quarter\nCharly Dang 010924537"
                              + "\nBrandon Nguyen 011499566\nColin Koo 010291241\nFelix Zhang 01042383"
                              + "\nGerianna Geminiano 010662522");
@@ -131,11 +127,11 @@ public class HManGame extends JPanel implements ActionListener {
         
         skip.setToolTipText("Press this button to skip the Hangman Game and go directly to the Color Game");
         
-        KeyStroke space = KeyStroke.getKeyStroke(' ');//supposed to be KeyEvent.VK_F1, 0 , true);
+        KeyStroke space = KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0 , true);
         inputMap = skipPanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
         inputMap.put(space, ACTION_KEY);
         skipPanel.setActionMap(actionMap);
-        
+	
         skipPanel.add(skip);
 
         skip.addActionListener(new ActionListener() {
@@ -147,6 +143,7 @@ public class HManGame extends JPanel implements ActionListener {
             }
         });
     }
+
     /*
     method: drawGame
     purpose: Draws the visual representation of the game, which includes
@@ -168,7 +165,7 @@ public class HManGame extends JPanel implements ActionListener {
             word.add(guessWord[i]);
             guessWord[i].setOpaque(true);
             guessWord[i].setBorder(BorderFactory.createMatteBorder(0, 0, 3, 0, Color.black));
-            guessWord[i].setToolTipText("Guess this word!!");
+			guessWord[i].setToolTipText("Guess this word!!");
         }
         centerPanel.add(word, BorderLayout.PAGE_END);
     }
@@ -181,7 +178,8 @@ public class HManGame extends JPanel implements ActionListener {
     private void drawTitle() {
         JLabel hangman = new JLabel("HANGMAN");
         JLabel time = new JLabel();
-        hangman.setToolTipText("Welcome to the Hangman Game!");
+		time.setToolTipText("The current time!");
+		hangman.setToolTipText("Welcome to the Hangman Game!");
         // adding the time
         time.setHorizontalAlignment(JLabel.CENTER);
         time.setFont(UIManager.getFont("Label.font").deriveFont(Font.BOLD, 12));
@@ -189,14 +187,12 @@ public class HManGame extends JPanel implements ActionListener {
             @Override
             public void actionPerformed(ActionEvent e) {
                 time.setText(DateFormat.getDateTimeInstance().format(new Date()));
-              
             }
         });
         timer.setRepeats(true);
         timer.setCoalesce(true);
         timer.setInitialDelay(0);
         timer.start();
-        time.setToolTipText("The current time!");
         titleBar.add(time, BorderLayout.LINE_END);
 
         //adding the stylized HANGMAN
@@ -207,7 +203,7 @@ public class HManGame extends JPanel implements ActionListener {
         points.setText("Points: " + score);
         points.setFont(new Font("Arial", Font.ITALIC, 14));
         points.setBorder(BorderFactory.createEmptyBorder(0, 100, 0, 10));
-        points.setToolTipText("Your current total of points: " + score);
+		points.setToolTipText("Your current total of points: " + score);
         titleBar.add(points, BorderLayout.CENTER);
     }
 
@@ -223,7 +219,7 @@ public class HManGame extends JPanel implements ActionListener {
             buttons[i].addActionListener(this);
             // buttons[i].setFont(new Font("Calibri", Font.PLAIN, 11));
             buttons[i].setMargin(new Insets(0, 0, 0, 0));
-            buttons[i].setToolTipText("Press " + letters[i]);
+			buttons[i].setToolTipText("Press " + letters[i]);
             btnPanel.add(buttons[i]);
         }
 
@@ -268,7 +264,6 @@ public class HManGame extends JPanel implements ActionListener {
                 engine.setScore(score);
             }
         }
-      
     }
 
     /*
@@ -365,8 +360,9 @@ public class HManGame extends JPanel implements ActionListener {
          */
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
+			super.setToolTipText("The gallows demand a body! Number of wrong guesses: " + getTries() + "!");
             Graphics2D g2 = (Graphics2D) g;
-            super.setToolTipText("The gallows demand a body! Number of wrong guesses: " + getTries() + "!");
+
             g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
             g2.setStroke(new BasicStroke(10));
@@ -408,7 +404,7 @@ public class HManGame extends JPanel implements ActionListener {
             if (wrongTries >= 6) {
                 g2.drawLine(299, 150, 283, 180);
             }
-            
+
             repaint();
         }
 
