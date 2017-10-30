0a1b63fbf3021f88735f8067c415e8a6cd30dba2
charlydang <charly_dang@yahoo.com>
2017-10-29 19:17:08 -0700

    Update ColorGameEngine.java

--- CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/ColorGameEngine.java
+++ CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/ColorGameEngine.java
@@ -1,12 +1,12 @@
 /** *************************************************************
- * file: Credits.java
+ * file: ColorGameEngine.java
  * author: Brandon Nguyen, Charly Dang, Colin Koo, Felix Zhang, Gerianna Geminiano
- * class: CS 245 � Programming Graphical User Interface
+ * class: CS 245 – Programming Graphical User Interface
  *
  * assignment: Swing Project v1.1
  * date last modified: 10/19/17
  *
- * purpose: This program is a "Point-and-click" Hangman and Color game. Using Swing,
+ * purpose: purpose: This program is a "Point-and-click" Hangman and Color game. Using Swing,
  * we created a game that is controlled by your mouse and keyboard. The user
  * will be able to play the classic Hangman game with 6 guesses, play a matching
  * color game with 5 rounds, see the top 5 high scores, and the credits. You will
@@ -16,138 +16,384 @@
  *************************************************************** */
 package cs245.v1.pkg0.pkg1;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import javax.swing.*;
-import javax.swing.KeyStroke;
-import javax.swing.JButton;
-import javax.swing.AbstractAction;
-import javax.swing.KeyStroke;
-import javax.swing.InputMap;
-public class Credits extends JPanel {
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-    private MainFrame main;
-    private JButton backButton = new JButton("Back");
-    private JLabel[] names = new JLabel[6];
-    private JPanel scores = new JPanel();
-    private JPanel buttons = new JPanel(new BorderLayout());
-    private static Action enterAction;
-    private static JFrame frame;
+public class ColorGameEngine {
 
-    /*
-    Constructor
-     */
-    public Credits() {
-
-        setLayout(new BorderLayout());
-        readCredits();
-        showCredits();
-        addBackButtons();
-        requestFocusInWindow();
-       
-
-    }
-
-    /* 
-    method: setMain
-    puspose: This method is used to set this instance of MainFrame to the current
-    main frame so that the swapview method can be accessed to change the
-    panel back to the main menu once the back button is pressed.
-     */
-    public void setMain(MainFrame panel) {
-        this.main = panel;
-    }
+    Circle[] circles = new Circle[5];
+    private int score = 0;
+    private boolean won;
+    private String input;
+    private String[] colors = {"red", "yellow", "green", "blue","purple"};
+    private String target;
+    private int rounds = 0;
 
 
     /*
-    method: showCredits
-    puspose: Displaying the Credits.
+        Constructor
      */
-    private void showCredits() {
-        scores.setLayout(new BoxLayout(scores, BoxLayout.Y_AXIS));
+    public ColorGameEngine() {
 
-        for (int i = 0; i < 6; i++) {
-            if (names[i] != null) {
-                scores.add(names[i]);
-            }
-        }
-        scores.setToolTipText("These are the names/brc ids of the project group memebers");
-        add(scores, BorderLayout.CENTER);
-    }
-
-    /*
-    method: readCredits
-    puspose:Reading the Credits from the array and adding
-    them to the JLabel array.
-     */
-    private void readCredits() {
-
-        String[] entries = {"Credits", "Brandon Nguyen 011499566", "Charly Dang 010924537", "Colin Koo 010291242",
-            "Felix Zhang 010423283", "Gerianna Geminiano 010662522"};
-        String[] input;
-
-        names[0] = new JLabel(entries[0]);
-        names[0].setFont(new Font("Papyrus", Font.BOLD, 30));
-        names[0].setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        for (int i = 1; i < 6; i++) {
-            input = entries[i].split(" ");
-            names[i] = new JLabel(input[0] + " " + input[1] + "........." + input[2]);
-            names[i].setFont(new Font("Papyrus", Font.BOLD, 24));
-            names[i].setAlignmentX(Component.CENTER_ALIGNMENT);
-        }
         
     }
 
     /*
-     method: addBackButtons
-    puspose: adding the back button.
+        method: getText
+        purpose: returns string of literal color word
      */
-    private void addBackButtons() {
-        String ACTION_KEY = "The Action";
-        Action actionListener = new AbstractAction() {
-      public void actionPerformed(ActionEvent actionEvent) {
-        String source = actionEvent.getActionCommand();
-        System.out.println("YOOOOO");
-        System.out.println("The command:" + source);
-        if(source==null){
-            JOptionPane.showMessageDialog(frame, "Winter Quarter\nCharly Dang 010924537"
-                             + "\nBrandon Nguyen 011499566\nColin Koo 010291241\nFelix Zhang 01042383"
-                             + "\nGerianna Geminiano 010662522");
-        } else {
-           System.exit(0);
-        }
-        }
-        };
-        KeyStroke escape = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, true);
-        InputMap inputMap = backButton.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
-        inputMap.put(escape, ACTION_KEY);
-        ActionMap actionMap = backButton.getActionMap();
-        actionMap.put(ACTION_KEY, actionListener);
-        backButton.setActionMap(actionMap);
-        
-        KeyStroke f1 = KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0 , true);//supposed to be KeyEvent.VK_F1, 0 , true);
-        inputMap = buttons.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
-        inputMap.put(f1, ACTION_KEY);
-        buttons.setActionMap(actionMap);
-        
-        backButton.setToolTipText("Press the bacl button to go back to the main menu");
-        buttons.add(backButton, BorderLayout.LINE_START);
-        buttons.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        add(buttons, BorderLayout.PAGE_END);
+    public String getText() {
+        int ran = new Random().nextInt(colors.length);
+        target = colors[ran];
+        return target;
 
-        backButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                main.startMenuTimer();
-                main.swapView("menu");
-            }
-        });
     }
-     // class EnterAction is an AbstractAction that defines what will occur
-    // when the enter key is pressed. 
     
+    public String getTarget(){
+        return target;
+    }
+
+    /*
+    method: setTarget
+    puspose: sets the color of the text
+     */
+    public void setTarget(int rnd) {
+        target = colors[rnd];
+        //System.out.println(target);
+    }
+
+    /*
+    method: getScore
+    puspose: returns score
+     */
+    public int getScore() {
+        return score;
+    }
+
+    /*
+    method: setScore
+    purpose: setter for the score variable
+     */
+    public void setScore(int x) {
+        score = x;
+    }
+
+    /*
+    method: matches
+    puspose: check if the circle and color of text match and updates varaibles
+     */
+    public void matches(String input) {
+        if (rounds <= 3) {
+            if (target.equals(input)) {
+                score += 100;
+                //System.out.println("Correct: " + score);
+                setScore(score);
+                rounds++;
+            } else {
+                //System.out.println("WRONG: " + score);
+                rounds++;
+            }
+        } else if (rounds == 4) {
+            if (target.equals(input)) {
+                score += 100;
+                //System.out.println("Correct: " + score);
+                rounds++;
+            } else {
+                //System.out.println("Wrong: " + score);
+                rounds++;
+            }
+        } else {
+            //System.out.println("Game is over");
+        }
+    }
+
+    /*
+    method: getRounds
+    puspose: returns number of rounds
+     */
+    public int getRounds() {
+        return rounds;
+    }
+
+    /*
+    method: resetRounds
+    puspose: sets the number of rounds to 0
+     */
+    public void resetRounds() {
+        rounds = 0;
+    }
+
+
+    /*
+    method: drawCircleAt
+    puspose: draws a circles at coordinates (x,y)
+     */
+    public Shape drawCircleAt(int x, int y) {
+        Shape circle = new Ellipse2D.Double(x, y, 50.0f, 50.0f);
+        return circle;
+    }
+
+    /*
+    method: getCircles
+    puspose: returns an array of the circles
+     */
+    public Shape[] getCircles(int width, int height) {
+        Shape[] shapes = new Shape[5];
+        int x, y, radius;
+
+        calcCirclePos(width, height);
+
+        for (int i = 0; i < circles.length; i++) {
+            x = circles[i].getXPos();
+            y = circles[i].getYPos();
+            radius = circles[i].getRadius();
+
+            shapes[i] = new Ellipse2D.Double(x, y, radius, radius);
+
+        }
+        return shapes;
+    }
+
+    /*
+    method: isWinner
+    puspose: returns a number depeding on if player wins or loses
+    1 = loss, 2 = win but not highscore, 3 = win and highscore
+     */
+    public int isWinner() {
+        int winType = 0;
+        int lowestScore = 0;
+
+        try {
+            File file = new File("HighScores.txt");
+            BufferedReader buff = new BufferedReader(new FileReader(file));
+            String line = buff.readLine();
+            // Gets lowest highscore
+            while ((line) != null) {
+                String[] splitted = line.split(" ");
+                lowestScore = Integer.parseInt(splitted[1]);
+                line = buff.readLine();
+            }
+            buff.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ColorGameEngine.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ColorGameEngine.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        if (score <= 40) { // If score is 40(all guesses used) or less then a loss
+            winType = 1;
+        } else if (score < lowestScore) {// If score is less than lowest highscore
+            winType = 2;
+        } else if (score >= lowestScore) { // If score is higher than lowest highscore
+            winType = 3;
+        }
+        return winType;
+    }
+
+    /*
+    method: updateHighScore
+    purpose: Gets players score and adds it to the highscore list if its in the top 5
+    First it reads the highscores file, adds it to an array with the new score, and 
+    then overrites old file.
+     */
+    public void updateHighScore(String name, int score) {
+
+        String scoreArr[] = new String[5];
+        try {
+            File f = new File("HighScores.txt");
+            BufferedReader br = new BufferedReader(new FileReader(f));
+
+            boolean replaced = false;
+            int i = 0;
+            // if File is empty
+            String line = br.readLine();
+            if (line.length() == 0) {
+                scoreArr[i] = name + " " + Integer.toString(score);
+                ++i;
+            } else {
+                while ((line) != null) {
+                    String[] splitted = line.split(" ");
+                    if ((Integer.parseInt(splitted[1]) <= score) && replaced == false) {
+                        if (i < 5) {
+                            scoreArr[i] = name + " " + Integer.toString(score);
+                            ++i;
+                        }
+                        if (i < 5) {
+                            scoreArr[i] = line;
+                            ++i;
+                        }
+                        replaced = true;
+                    } else {
+                        if (i < 5) {
+                            scoreArr[i] = line;
+                            ++i;
+                        }
+                    }
+                    line = br.readLine();
+                }
+            }
+            if (i < 5) {
+                while (i < 5) {
+                    scoreArr[i] = "AAA 0";
+                    ++i;
+                }
+            }
+
+            i = 0;
+            br.close();
+            f.delete();
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter("HighScores.txt"));
+            for (int n = 0; n < scoreArr.length; n++) {
+                bw.write(scoreArr[n]);
+                bw.newLine();
+                bw.flush();
+            }
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+    method: calcCirclePos
+    purpose: 
+    This is used to calculate the positions of the circles with help
+    from the intersects method. Takes in the width and the height of the 
+    JPanel and draws circles within the bounds of the JPanel.
+    @returns a circle array to the circle panel class.
+     */
+    public void calcCirclePos(int xHeight, int yHeight) {
+    
+        circles = new Circle[5];
+        int windowH = yHeight;
+        int windowW = xHeight;
+        int counter = 0;
+        Random rand = new Random();
+        Circle tempCirc;
+        int tempX, tempY;
+        boolean done = false;
+
+        /*
+        while the circle array is not filled, keep picking random numbers for
+        coordinates of the circle. Then check if the random circle overlaps with 
+        any circle thats already in the list. if so, repick new random coordinates.
+         */
+        while (!done) {
+            tempX = rand.nextInt(windowW);
+            tempY = rand.nextInt(windowH);
+            tempCirc = new Circle(tempX, tempY);
+
+            if (!containsInter(tempCirc)) {
+                circles[counter] = tempCirc;
+                counter++;
+            }
+            
+            if (counter == 5) {
+                done = true;
+            }
+
+        }
+
+    }
+
+
+    /*
+    method: containsInter
+    puspose: method to check if target circle has an intersection
+    within the circle array.
+     */
+    private boolean containsInter(Circle target) {
+
+        for (int i = 0; i < circles.length; i++) {
+            if (circles[i] != null) {
+                if (target.intersects(circles[i])) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /*
+    object to hold the circle information.
+     */
+    public static class Circle {
+
+        private int xPos, yPos;
+        private int xCenter, yCenter;
+        private final int radius = 70;
+
+        public Circle(int xPos, int yPos) {
+            this.xPos = xPos;
+            this.yPos = yPos;
+            xCenter = xPos + 35;
+            yCenter = yPos + 35;
+        }
+
+        /*
+        method: intersects
+        purpose: checks if two circles are intersecting given the two
+        circle objects. 
+         */
+        public boolean intersects(Circle comp) {
+            double radiSum = (radius * 2) + 10;
+            double x0x1 = Math.pow((comp.getXCenter() - this.getXCenter()), 2);
+            double y0y1 = Math.pow((comp.getYCenter() - this.getYCenter()), 2);
+            double sum = x0x1 + y0y1;
+            double distance = Math.sqrt(sum);
+
+            if (radiSum >= distance) {
+                return true;
+            }
+            return false;
+        }
+
+        public int getXCenter() {
+            return xCenter;
+        }
+
+        public int getYCenter() {
+            return yCenter;
+
+        }
+
+        public int getXPos() {
+            return xPos;
+        }
+
+        public int getYPos() {
+            return yPos;
+        }
+
+        public int getRadius() {
+            return radius;
+        }
+
+        public void setXPos(int xPos) {
+            this.xPos = xPos;
+            xCenter = xPos + 25;
+        }
+
+        public void setYPos(int yPos) {
+            this.yPos = yPos;
+            yCenter = yPos + 25;
+        }
+
+        public String toString() {
+            String retString = this.xCenter + " " + this.yCenter;
+            return retString;
+        }
+
+    }
 }
