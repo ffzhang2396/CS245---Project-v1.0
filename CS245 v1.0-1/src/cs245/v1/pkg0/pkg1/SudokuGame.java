00157f06ced897cb767fbc9956418a8cc2d2e807
charlydang <charly_dang@yahoo.com>
2017-10-30 22:16:38 -0700

    Update SudokuGame.java

--- CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/SudokuGame.java
+++ CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/SudokuGame.java
@@ -17,13 +17,14 @@ import javax.swing.*;
  *
  */
 public class SudokuGame extends JPanel {
-    private ColorGameEngine cEngine; 
+
+    private ColorGameEngine cEngine;
     private SudokuGameEngine engine;
     private MainFrame main;
     private JTextField[][] boxes = new JTextField[9][9];
     private JPanel board = new JPanel();
 
-    public SudokuGame(SudokuGameEngine engine,ColorGameEngine cEngine) {
+    public SudokuGame(SudokuGameEngine engine, ColorGameEngine cEngine) {
         this.engine = engine;
         this.cEngine = cEngine;
         setLayout(new BorderLayout());
@@ -41,19 +42,29 @@ public class SudokuGame extends JPanel {
     }
 
     /*
+    method: startNewGame
+    purpose: Resets the game.
+     */
+    public void startNewGame() {
+        engine.setScore(540); // reset the score back to 540
+        // NEED TO ADD MORE CODE TO RESET EVERYTHINE ELSE
+        
+    }
+
+    /*
     For the sudoku board we are going to use a
     9x9 gridLayout for the inner JPanel thats going to be in the center of the 
     outer JPanel which is an instance of this class.
      */
     private void createBoard() {
         board.setLayout(new GridLayout(9, 9));
-        
-        
 
         for (int i = 0; i < boxes.length; i++) {
             for (int j = 0; j < boxes.length; j++) {
 
                 board.add(boxes[i][j]);
+                board.setToolTipText("Sudoku Board! Come play!");
+                boxes[i][j].setToolTipText("The current coordiantes are: ["+i+"]["+j+"]" );
             }
         }
 
@@ -69,8 +80,9 @@ public class SudokuGame extends JPanel {
     private void titleBar() {
         JPanel title = new JPanel(new BorderLayout());
         JLabel sudoku = new JLabel("SUDOKU");
+        sudoku.setToolTipText("Time to commit SUDOKU!!");
         JLabel time = new JLabel();
-        
+
         //adding the time
         time.setHorizontalAlignment(JLabel.CENTER);
         time.setFont(UIManager.getFont("Label.font").deriveFont(Font.BOLD, 12));
@@ -84,9 +96,9 @@ public class SudokuGame extends JPanel {
         timer.setCoalesce(true);
         timer.setInitialDelay(0);
         timer.start();
+        time.setToolTipText("The current Date!");
         title.add(time, BorderLayout.LINE_END);
-        
-        
+        title.setToolTipText("Welcome to the Sudoku Game!");
         //adding stylized sudoku name
         sudoku.setFont(new Font("Papyrus", Font.BOLD, 18));
         title.add(sudoku, BorderLayout.LINE_START);
@@ -100,27 +112,79 @@ public class SudokuGame extends JPanel {
      */
     private void submitButton() {
         JButton submit = new JButton("Submit");
+        submit.setToolTipText("Submit all of your entries");
         JPanel buttons = new JPanel(new BorderLayout());
-        
+
         //formatting
         buttons.setBorder(BorderFactory.createEmptyBorder(10, 25, 25, 25));
         buttons.add(submit, BorderLayout.PAGE_END);
+        buttons.setToolTipText("Please do something.");
         add(buttons, BorderLayout.LINE_START);
         
         //adding actionListener
         submit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-               //add actionlistener actions for submitting
-               //
-               // Game Over Score is Sudoku Score + previous games Score
-               engine.setScore(cEngine.getScore()+engine.getFinalScore());
+
+             
+            if(checkInput()){
                System.out.println("Sudoku Score: " + engine.getFinalScore());
+               engine.setScore(cEngine.getScore()+engine.getFinalScore());
+               System.out.println("Final Score: " + engine.getFinalScore());
                main.gameOverMessage();
                main.swapView("over");
             }
-            
+            }
         });
+    }
+
+    /*
+    checks the boxes of sudoku puzzle
+    and compares with the answer board. will highlight the boxes
+    red if the answer does not match, green if it is correct. need to
+   ol choose a less bright color.
+     */
+    private boolean checkInput() {
+        int[][] answers = engine.getAns();
+        int[][] wrongAns = engine.getWrong(); // Array that tells which box had wrong answer
+        String ans = "";
+        JFrame pop = new JFrame();
+        boolean finish = true;
+
+        for (int i = 0; i < boxes.length; i++) {
+            for (int j = 0; j < boxes.length; j++) {
+
+                try {
+                    ans = boxes[i][j].getText().replaceAll("\\s+", "");
+
+                    if (Integer.parseInt(ans) == answers[i][j]) {
+                        boxes[i][j].setBackground(Color.green);
+                    } else {
+                        if (wrongAns[i][j] == 0) {
+                            engine.setScore(engine.getFinalScore() - 10);
+                            wrongAns[i][j] = 1;
+                        }
+                        finish = false;
+                        boxes[i][j].setBackground(Color.red);
+                    }
+                } catch (NumberFormatException n) {
+                    if (wrongAns[i][j] == 0) {
+                        engine.setScore(engine.getFinalScore() - 10);
+                        wrongAns[i][j] = 1;
+                    }
+                    finish = false;
+                    boxes[i][j].setBackground(Color.red);
+                    continue;
+                }
+
+            }
+        }
+        
+        // Update wrong boxes array 
+        //Meant so that can't be dedcuted more than once for each box
+        engine.setWrong(wrongAns);
+        System.out.println("Score: " + engine.getFinalScore());
+        return finish;
     }
 
     /*
@@ -129,13 +193,13 @@ public class SudokuGame extends JPanel {
      */
     private void quitButton() {
         JButton quit = new JButton("Quit");
+        quit.setToolTipText("Press this button to quit the game.");
         JPanel btns = new JPanel(new BorderLayout());
-        
+        btns.setToolTipText("Please do something.");
         //formatting
         btns.setBorder(BorderFactory.createEmptyBorder(10, 25, 25, 25));
         btns.add(quit, BorderLayout.PAGE_END);
         add(btns, BorderLayout.LINE_END);
-        
 
         //adding actionListener
         quit.addActionListener(new ActionListener() {
@@ -144,11 +208,11 @@ public class SudokuGame extends JPanel {
                 // If choose to quit then Socuku score is 0
                 // So Ending score is previous games Score
                 engine.setScore(cEngine.getScore());
-                System.out.println("Sudoku Score: " + engine.getFinalScore());
+                System.out.println("Final Score: " + engine.getFinalScore());
                 main.gameOverMessage();
                 main.swapView("over");
             }
-            
+
         });
     }
 
@@ -158,30 +222,31 @@ public class SudokuGame extends JPanel {
      */
     private void initBoard() {
         int[][] board = engine.getBoard();
-        
-        
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                boxes[i][j] = new JTextField();
+                boxes[i][j] = new JTextField(1);
+                boxes[i][j].setToolTipText("The current coordiantes are: ["+i+"]["+j+"]" );
                 if (board[i][j] != 0) {
                     boxes[i][j].setText(Integer.toString(board[i][j]));
                     boxes[i][j].setEditable(false);
                 } else {
                     boxes[i][j].setText(" ");
                     boxes[i][j].setEditable(true);
-                }                              
+                }
             }
         }
-        
         beautifyBorders();
     }
-    
+
+    /*
+    method to draw the black bars for sudoku
+    to make the board more appealling to the eye.
+     */
     private void beautifyBorders() {
-        
+
         for (int i = 0; i < boxes.length; i++) {
             for (int j = 0; j < boxes.length; j++) {
-                
                 if (j == 2 || j == 5) {
                     boxes[i][j].setBorder(BorderFactory.createMatteBorder(1, 1, 1, 3, Color.black));
                 } else if (i == 2 || i == 5) {
@@ -189,39 +254,13 @@ public class SudokuGame extends JPanel {
                 } else {
                     boxes[i][j].setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.black));
                 }
-                
                 boxes[2][5].setBorder(BorderFactory.createMatteBorder(1, 1, 3, 3, Color.black));
                 boxes[2][2].setBorder(BorderFactory.createMatteBorder(1, 1, 3, 3, Color.black));
                 boxes[5][2].setBorder(BorderFactory.createMatteBorder(1, 1, 3, 3, Color.black));
                 boxes[5][5].setBorder(BorderFactory.createMatteBorder(1, 1, 3, 3, Color.black));
-                
+
                 boxes[i][j].setHorizontalAlignment(JTextField.CENTER);
             }
         }
     }
-
-    /*
-    need to add a separate action listener that listens to 
-    when the JtextFields are modified with user input.
-    This listener needs to be different so that it does not 
-    mess with the action listeners for the buttons.
-     */
-    private class gridListener implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            JTextField src = (JTextField) e.getSource();
-
-        }
-
-        /*
-        this method is used for finding the x and y values
-        for the engine to check if the value is a valid input.x
-         */
-        private void findSrc() {
-
-        }
-
-    }
-
 }
