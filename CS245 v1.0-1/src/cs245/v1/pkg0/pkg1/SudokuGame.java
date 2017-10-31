e6504171841e134167907cde31f81780bbe21bc9
Charly Dang <cadang@cpp.edu>
2017-10-30 18:35:10 -0700

    Revert "Tool Tips added"
    This reverts commit 2c9ee7e44aa83ae7ca5c336664e96d8bf58e8816.

--- CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/SudokuGame.java
+++ CS245 v1.0-1/src/cs245/v1/pkg0/pkg1/SudokuGame.java
@@ -63,8 +63,6 @@ public class SudokuGame extends JPanel {
             for (int j = 0; j < boxes.length; j++) {
 
                 board.add(boxes[i][j]);
-                board.setToolTipText("Sudoku Board! Come play!");
-                boxes[i][j].setToolTipText("The current coordiantes are: ["+i+"]["+j+"]" );
             }
         }
 
@@ -80,7 +78,6 @@ public class SudokuGame extends JPanel {
     private void titleBar() {
         JPanel title = new JPanel(new BorderLayout());
         JLabel sudoku = new JLabel("SUDOKU");
-        sudoku.setToolTipText("Time to commit SUDOKU!!");
         JLabel time = new JLabel();
 
         //adding the time
@@ -96,9 +93,8 @@ public class SudokuGame extends JPanel {
         timer.setCoalesce(true);
         timer.setInitialDelay(0);
         timer.start();
-        time.setToolTipText("The current Date!");
         title.add(time, BorderLayout.LINE_END);
-        title.setToolTipText("Welcome to the Sudoku Game!");
+
         //adding stylized sudoku name
         sudoku.setFont(new Font("Papyrus", Font.BOLD, 18));
         title.add(sudoku, BorderLayout.LINE_START);
@@ -112,15 +108,13 @@ public class SudokuGame extends JPanel {
      */
     private void submitButton() {
         JButton submit = new JButton("Submit");
-        submit.setToolTipText("Submit all of your entries");
         JPanel buttons = new JPanel(new BorderLayout());
 
         //formatting
         buttons.setBorder(BorderFactory.createEmptyBorder(10, 25, 25, 25));
         buttons.add(submit, BorderLayout.PAGE_END);
-        buttons.setToolTipText("Please do something.");
         add(buttons, BorderLayout.LINE_START);
-        
+
         //adding actionListener
         submit.addActionListener(new ActionListener() {
             @Override
@@ -193,9 +187,8 @@ public class SudokuGame extends JPanel {
      */
     private void quitButton() {
         JButton quit = new JButton("Quit");
-        quit.setToolTipText("Press this button to quit the game.");
         JPanel btns = new JPanel(new BorderLayout());
-        btns.setToolTipText("Please do something.");
+
         //formatting
         btns.setBorder(BorderFactory.createEmptyBorder(10, 25, 25, 25));
         btns.add(quit, BorderLayout.PAGE_END);
@@ -226,7 +219,6 @@ public class SudokuGame extends JPanel {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 boxes[i][j] = new JTextField(1);
-                boxes[i][j].setToolTipText("The current coordiantes are: ["+i+"]["+j+"]" );
                 if (board[i][j] != 0) {
                     boxes[i][j].setText(Integer.toString(board[i][j]));
                     boxes[i][j].setEditable(false);
